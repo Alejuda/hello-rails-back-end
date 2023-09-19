8ce2c55ceda9ac802426452e99b95b3db12cf246
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-19 13:43:33 -0300

    Fix controller error and define the api endpoint

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,4 +1,4 @@
-class GreetingsController < ApplicationController
+class Api::GreetingsController < ApplicationController
   def random_greeting
     random_message = Message.find(rand(1..5))
     if random_message
